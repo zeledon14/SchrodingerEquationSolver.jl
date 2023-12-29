d8bd5fe7526fa293fb288da26af48105f7136a24
zeledon14 <zeledon14@gmail.com>
2023-12-29 14:31:31 -0500

    re organize

--- src/SchrodingerEquationSolver.jl
+++ src/SchrodingerEquationSolver.jl
@@ -1,5 +1,9 @@
 module SchrodingerEquationSolver
 
-# Write your package code here.
-
+include("Grids.jl");
+include("MathUtils.jl");
+include("Potentials.jl");
+include("IntegralNumericalMethods.jl");
+include("1DSchrodingerEquationSolver.jl");
+include("Hydrogen.jl");
 end
