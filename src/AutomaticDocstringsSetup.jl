8cc3d5bae35c9c0af1f911465e1f0488fc17ba00
zeledon14 <zeledon14@gmail.com>
2023-12-29 14:31:50 -0500

    add instructions

--- src/AutomaticDocstringsSetup.jl
+++ src/AutomaticDocstringsSetup.jl
@@ -4,3 +4,8 @@ AutomaticDocstrings.options[:min_args]=1
 AutomaticDocstrings.options[:arg_types_in_desc]= true
 AutomaticDocstrings.options[:args_header]="**Inputs:**"
 AutomaticDocstrings.options[:arg_types_in_header]= true
+
+#add @autodoc on top of the function
+#On Julia  repl
+#include("src/AutomaticDocstringsSetup.jl")
+#autodoc("path_to_file")
