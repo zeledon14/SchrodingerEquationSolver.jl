f19f8acd9f841728495a337120214b4f462bed7d
zeledon14 <zeledon14@gmail.com>
2024-01-15 16:13:04 -0500

    update harmonic eigen search

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,5 @@ using Test
 @testset "SchrodingerEquationSolver.jl" begin
     include("uS1HydrogenTest.jl")
     include("OneDSchrodingerEquationSolverTest.jl")
+    include("HarmincOcillatorEigenStatesSearchTest.jl")
 end
