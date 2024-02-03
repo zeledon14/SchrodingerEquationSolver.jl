7ca834625c3633b4da5d11c8e5f974d6dbca0283
zeledon14 <zeledon14@gmail.com>
2024-02-03 14:37:01 -0500

    fix bug for odd Z

--- src/AtomBasisSet.jl
+++ src/AtomBasisSet.jl
@@ -10,6 +10,7 @@ mutable struct orbital
     l::Int64;
     name::String;
     E::Float64;
+    occu::Float64;
     u:: Vector{Float64};
 end
 
@@ -26,22 +27,31 @@ function init_atom_basis_set(Z::Int64, grid::Vector{Float64})::atom_basis_set
     N::Int64=sizeof(grid)[1];
     electron_capacity::Int64=0;
     numb_orbitals::Int64=0
-    #finding the number of orbitals needed given Z
+    remaining_electrons::Int64= Int64(Z)
+    #finding number of orbitals needed
     for i_orbi in atomic_numbers
-        if electron_capacity + i_orbi.max_number_of_electrons <= Z
-            electron_capacity += i_orbi.max_number_of_electrons;
+        if remaining_electrons > 0
+            remaining_electrons -= i_orbi.max_number_of_electrons;
             numb_orbitals+=1;
         end  
     end
     #init orbitals
     #initl with the u wave function as zeroes
-    orbitals=[orbital(0,0,"",0,zeros(N)) for _ in (1:numb_orbitals)]
+    orbitals=[orbital(0,0,"",0,0,zeros(N)) for _ in (1:numb_orbitals)]
+    remaining_electrons= Int64(Z)
     #fill orbitals with information
     for i in (1:numb_orbitals) 
         orbitals[i].n=atomic_numbers[i].n
         orbitals[i].l=atomic_numbers[i].l
         orbitals[i].name=atomic_numbers[i].orbital_name
         orbitals[i].E= -0.5*(Z^2)/(atomic_numbers[i].n^2)
+        if remaining_electrons >= atomic_numbers[i].max_number_of_electrons
+            orbitals[i].occu= atomic_numbers[i].max_number_of_electrons
+            remaining_electrons -= atomic_numbers[i].max_number_of_electrons
+        else
+            orbitals[i].occu=remaining_electrons
+        end
+
     end
 
     return atom_basis_set(grid,orbitals)
