193fbc9ecd1121881a1d2c179166f04a7d026618
zeledon14 <zeledon14@gmail.com>
2025-02-04 20:41:12 -0500

    add backward integration for poisson

--- src/OneDPoissonEquationSolver.jl
+++ src/OneDPoissonEquationSolver.jl
@@ -85,4 +85,41 @@ function solver_exponential_grid(Z::Int64, density::Vector{Float64},
     return V_hartree
 end
 
+function solver_exponential_grid_forward_back(Z::Int64, density::Vector{Float64},
+    grid_stru::Any)::Tuple{Vector{Float64},Vector{Float64}}
+
+    a::Float64=grid_stru.a;
+    b::Float64=grid_stru.b;
+    grid_i::Vector{Float64}=grid_stru.grid_i;
+    grid::Vector{Float64}=grid_stru.grid;
+    g::Vector{Float64}=(-4.0*pi).*density.*grid;
+    gw::Vector{Float64}=(((a^2*b^2).*exp.(1.5.*b.*grid_i))).*g;
+    #g::Vector{Float64}=(-1).*density.*grid;
+    fw::Vector{Float64}= (0.25*b^2).*ones(Float64, size(g)[1]);
+    w1::Float64=grid[1]*exp(-0.5*b*grid_i[1])
+    dw1::Float64= a*b*exp(0.5*b*grid_i[1]) - 0.5*b*grid[1]*exp(-0.5*b*grid_i[1])
+    w_end::Float64=Z*exp(-0.5*b*grid_i[end])
+    dw_end::Float64= -0.5*b*Z*exp(-0.5*b*grid_i[end])
+
+    #do forward integration of poisson to get U_hartree
+    vv_hartree=IntegralNumericalMethods.integrate_second_order_DE_RK4_PCABM5_direct_initial(grid_i,gw,fw,
+    w1,dw1)
+
+    #do backward integration of poisson to get U_hartree
+    vv_hartree_bcwd=IntegralNumericalMethods.integrate_second_order_DE_RK4_PCABM5_direct_initial(reverse(grid_i),reverse(gw),reverse(fw),
+    w_end,dw_end)
+    #vv_hartree= IntegralNumericalMethods.integrate_second_order_DE_RK4_PCABM5(grid,g,f,
+    #init_valu1_fwrd,init_valu2_fwrd);
+    U_hartree= vv_hartree.*exp.((0.5*b).*grid_i);
+    U_hartree_bcwd= reverse(vv_hartree_bcwd).*exp.((0.5*b).*grid_i);
+    #set boudary condtion over U_hartree
+    a= (Z - U_hartree[end])/grid[end]
+    U_hartree= U_hartree .+ a.*grid
+    #transform into V_hartree
+    V_hartree=U_hartree./grid#[U_hartree[i]/xi for (i,xi) in enumerate(grid)]
+    V_hartree_bcwd=U_hartree_bcwd./grid
+    #return U_hartree
+    return V_hartree, V_hartree_bcwd
+end
+
 end
